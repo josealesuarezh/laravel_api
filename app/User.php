75b54426da7e63fe89024cfc4686d487efea5fab
joseasuarez <josealesuarezh@gmail.com>
2021-01-26 16:15:58 -0500

    adding middleware client.credentials

--- app/User.php
+++ app/User.php
@@ -6,10 +6,11 @@ use App\Transformers\UserTransformer;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable,SoftDeletes;
+    use Notifiable,SoftDeletes,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
